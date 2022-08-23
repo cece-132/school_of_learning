0893ef867ef5e52d298e7baa7ab404909912fbc7
Candace Eckels <candace.eckels@gmail.com>
2022-08-23 15:29:39 -0600

    Teacher Index and Show Page

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -2,4 +2,8 @@ class TeachersController < ApplicationController
   def index
     @teachers = Teacher.all
   end
+
+  def show
+    @teacher = Teacher.find(params[:teacher_id])
+  end
 end
--- spec/features/teachers/show_spec.rb
+++ spec/features/teachers/show_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Teacher do
   describe '#show page' do
-    it 'shows teacher data, including id and updated/created' do
+    it 'can navigate to #show page' do
       teacher1 = Teacher.create!(name: "Phyllis Waters", license_issued: Time.now,
         renew_license: false, max_students: 32)
       teacher2 = Teacher.create!(name: "Paul Whitemon", license_issued: Time.now,
@@ -16,8 +16,34 @@ RSpec.describe Teacher do
         expect(page).to have_link("Phyllis Waters")
 
         click_link "Phyllis Waters"
-        expect(current_path).to eq("/teachers/")
+        expect(current_path).to eq("/teachers/#{teacher1.id}")
       end
     end
+
+    it 'can show the teacher information' do
+      teacher1 = Teacher.create!(name: "Phyllis Waters", license_issued: Time.now,
+        renew_license: false, max_students: 32)
+      teacher2 = Teacher.create!(name: "Paul Whitemon", license_issued: Time.now,
+        renew_license: false, max_students: 32)
+      teacher3 = Teacher.create!(name: "Peter Rosas", license_issued: Time.now,
+          renew_license: false, max_students: 32)
+
+      visit "/teachers"
+      
+      within ".teachers" do
+        click_link "Phyllis Waters"
+        expect(current_path).to eq("/teachers/#{teacher1.id}")
+      end
+
+      expect(page).to have_content("Name: Phyllis Waters")
+      expect(page).to have_content("License Issued: #{teacher1.license_issued.strftime("%m/%d/%y")}")
+      expect(page).to have_content("License Renewal: No")
+      expect(page).to have_content("Start Date: #{teacher1.created_at.strftime("%m/%d/%y")}")
+      expect(page).to have_content("Last Update: #{teacher1.updated_at.strftime("%m/%d/%y")}")
+
+      expect(page).to_not have_content("Name: Paul Whitemon")
+      expect(page).to_not have_content("Name: Peter Rosas")
+    end
+    
   end
 end
