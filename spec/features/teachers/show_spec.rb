3ba8c15789a390f361d1a627edbec59722184339
Candace Eckels <candace.eckels@gmail.com>
2022-08-24 11:30:52 -0600

    Added total student functionality toteacher show page

--- spec/features/teachers/show_spec.rb
+++ spec/features/teachers/show_spec.rb
@@ -44,6 +44,24 @@ RSpec.describe Teacher do
       expect(page).to_not have_content("Name: Paul Whitemon")
       expect(page).to_not have_content("Name: Peter Rosas")
     end
+
+    it 'shows the number of students for a teacher' do
+      teacher1 = Teacher.create!(name: "Phyllis Waters", license_issued: Time.now,
+        renew_license: false, max_students: 32)
+      teacher2 = Teacher.create!(name: "Paul Whitemon", license_issued: Time.now,
+        renew_license: false, max_students: 32)
+
+      student1 = Student.create!(name: "Quincey Jones", otg: false, max_classes: 6,
+                                teacher_id: teacher1.id)
+      student2 = Student.create!(name: "Aliya Blackmon", otg: false, max_classes: 8,
+                                teacher_id: teacher2.id)
+      student3 = Student.create!(name: "Prince Miles", otg: false, max_classes: 5,
+                                teacher_id: teacher1.id)
+
+      visit "/teachers/#{teacher1.id}"
+
+      expect(page).to have_content("Current Number of Students: #{teacher1.students.count}")
+    end
     
   end
 end
