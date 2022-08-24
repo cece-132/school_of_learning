94a9e4657d9184abb29f0827dbe0d1aa9251e491
Candace Eckels <candace.eckels@gmail.com>
2022-08-24 13:45:38 -0600

    Completes sort

--- spec/features/teachers/show_spec.rb
+++ spec/features/teachers/show_spec.rb
@@ -63,5 +63,31 @@ RSpec.describe Teacher do
       expect(page).to have_content("Current Number of Students: #{teacher1.students.count}")
     end
     
+    it 'has a navbar links' do
+      teacher1 = Teacher.create!(name: "Phyllis Waters", license_issued: Time.now,
+        renew_license: false, max_students: 32)
+      teacher2 = Teacher.create!(name: "Paul Whitemon", license_issued: Time.now,
+        renew_license: false, max_students: 32)
+
+      student1 = Student.create!(name: "Quincy Jones", otg: false, max_classes: 6,
+                                teacher_id: teacher1.id)
+      student2 = Student.create!(name: "Aliya Blackmon", otg: false, max_classes: 8,
+                                teacher_id: teacher2.id)
+      student3 = Student.create!(name: "Prince Miles", otg: false, max_classes: 5,
+                                teacher_id: teacher1.id)
+
+      visit "/teachers/#{teacher1.id}"
+
+      expect(page).to have_link("Students")
+      click_on "Students"
+      expect(current_path).to eq("/students")
+
+      visit "/students"
+
+      expect(page).to have_link("Teachers")
+      click_on "Teachers"
+      expect(current_path).to eq("/teachers")
+    end
+
   end
 end
