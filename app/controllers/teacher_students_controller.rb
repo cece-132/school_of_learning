78c95ddfe3cf6219f156c1189554b7eb912e509a
Candace Eckels <candace.eckels@gmail.com>
2022-08-25 17:10:04 -0600

    Teacher student creation

--- app/controllers/teacher_students_controller.rb
+++ app/controllers/teacher_students_controller.rb
@@ -1,7 +1,21 @@
 class TeacherStudentsController < ApplicationController
   def index
-    #can probably have within TeachersControllers through if-statement
     @teacher = Teacher.find(params[:teacher_id])
     @students = @teacher.students
+  end
+
+  def new
+    @teacher = Teacher.find(params[:teacher_id])
+  end
+
+  def create
+    new_student = Student.create(teacher_student_params)
+    redirect_to "/teachers/#{params[:teacher_id]}/students"
+  end
+
+  private
+
+  def teacher_student_params
+    params.permit(:name, :otg, :max_classes, :teacher_id)
   end
 end
