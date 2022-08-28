ca1cb27880854f4a918b07b0a8574fc5a5e42845
Candace Eckels <candace.eckels@gmail.com>
2022-08-27 19:21:40 -0600

    Instance method: student_classes(search_params)

--- spec/models/teacher_spec.rb
+++ spec/models/teacher_spec.rb
@@ -13,18 +13,20 @@ RSpec.describe Teacher, type: :model do
   end
 
   describe 'instance methods' do
-    it "can sort students by number of max classes" do
-      teacher1 = Teacher.create!(name: "Phyllis Waters", license_issued: Time.now,
-        renew_license: false, max_students: 32)
-      student1 = Student.create!(name: "Quincy Jones", otg: false, max_classes: 6,
-                          teacher_id: teacher1.id)
-      student2 = Student.create!(name: "Aliya Blackmon", otg: false, max_classes: 8,
-                          teacher_id: teacher1.id)
-      student3 = Student.create!(name: "Prince Miles", otg: true, max_classes: 5,
-                          teacher_id: teacher1.id)
-        
-      expected = ([student1, student2])
-      expect(teacher1.student_classes("6")).to eq(expected)
+    describe 'student_classes(search_params' do
+      it "can find students with classes >= search_params" do
+        teacher1 = Teacher.create!(name: "Phyllis Waters", license_issued: Time.now,
+          renew_license: false, max_students: 32)
+        student1 = Student.create!(name: "Quincy Jones", otg: false, max_classes: 6,
+                            teacher_id: teacher1.id)
+        student2 = Student.create!(name: "Aliya Blackmon", otg: false, max_classes: 8,
+                            teacher_id: teacher1.id)
+        student3 = Student.create!(name: "Prince Miles", otg: true, max_classes: 5,
+                            teacher_id: teacher1.id)
+          
+        expected = ([student1, student2])
+        expect(teacher1.student_classes("6")).to eq(expected)
+      end
     end
   end
 end
